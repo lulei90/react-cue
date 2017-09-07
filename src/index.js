1b36c4e97c49cc1966bb62a5f45b66988a1d3da4
lulei90 <lulei90@qq.com>
2017-09-07 14:28:23 +0800

    新增alert confirm快捷方法

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import CueBox from './CueBox'
 import React from 'react'
+import Model from './Model'
 import './index.less'
 const CueBoxCtrl =CueBox.renderBox();
 const Cue = CueBoxCtrl.create({
-    msg(content,time=3000,icon){
+    msg(content,time,icon){
         return this.add({
             content,
             time,
             icon
+        })
+    },
+    alert(content,click,title){
+        return this.add({
+            component:Model,
+            content,
+            title,
+            hasMask:true,
+            actions:[{
+                text: '确认',
+                color:'#108ee9',
+                //按钮的点击事件，默认带参数close方法用于关闭弹框
+                click: click
+            }]
+        })
+    },
+    confirm({content,actions,title,enterName,leaveName}){
+        return this.add({
+            component:Model,
+            content,
+            title,
+            enterName,
+            leaveName,
+            hasMask:true,
+            actions
         })
     },
     loading(content,hasMask=true,keep=true){
@@ -15,10 +41,7 @@ const Cue = CueBoxCtrl.create({
             content,
             icon:(<div className="cue-loader"></div>),
             hasMask,
-            keep,
-            close(){
-                console.log('loading 消失了')
-            }
+            keep
         })
     }
 });
